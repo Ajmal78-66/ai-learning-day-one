f9bc02c9f43f92f63e6bf942831d659c858486e0
Ajmal P.N. <ajmalnajeeb2009@gmail.com>
2026-09-11 19:49:42 +0530

    Day 1: Python basics

---
+++ DayOne/PrintAndInput.py
@@ -0,0 +1,16 @@
+# print and input
+
+print("victus login ")
+UserName = input("pls enter your name : ")
+print("welcome " + UserName + " to victus")
+age = int(input(UserName + " enter your age : "))
+conform = print("thank you for join with victus " + UserName )
+
+# if and else condition
+
+num = int(input("pls enter you age to verify : "))
+
+if age>18:
+    print(UserName + " you are eligible for the duty")
+else :
+    print("sorry" + UserName + "you are not eligible for the duty")
